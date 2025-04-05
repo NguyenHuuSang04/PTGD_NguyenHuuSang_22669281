30abe163ade8a1fdb51cde4bffaf809ef916f2ed
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-04-05 20:02:45 +0700

    Tuan 07: 3.Datatable

--- Tuan_07/src/components/Dashboard/Detailed.jsx
+++ Tuan_07/src/components/Dashboard/Detailed.jsx
@@ -7,6 +7,82 @@ import './Detailed.css';
 
 function Detailed() {
 
+    const [customers] = useState(data.customers); // gắn dữ liệu từ json
+    const [selectedCustomers, setSelectedCustomers] = useState([]); //state để lưu các hàng được chọn
+
+    //template để hiển thị ảnh avatar
+    const avatarBodyTemplate = (rowData) => {
+        return <img src={rowData.avatar} alt={rowData.name} className="w-10 h-10 rounded-full" />
+    }
+
+    // hàm hiển thị biểu tượng cây bút
+    const editBodyTemplate = (rowData) => {
+        return (
+
+            <button className="button p-0" onClick={() => handleEdit(rowData)}>
+                <img src="/src/img/create.png" alt="Edit" className="w-5 h-5" />
+            </button>
+
+        );
+    };
+
+    // hàm xử lý kkhi nhấn vào biểu tựng cây bút
+    const handleEdit = (rowData) => {
+        console.log("Edit row: ", rowData);
+        //add logic
+    }
+
+    return (
+        < >
+            <div className="flex mt-5 mb-5 justify-between ">
+                <div className="flex  items-center">
+                    <img src="/src/img/File text 1.png" alt="" className="w-7 h-7" />
+                    <h1 className="font-bold ml-2 text-xl">Detailed report</h1>
+                </div>
+
+                <div className="flex ">
+                    <button className="flex mr-6 border border-pink-400 hover:bg-pink-200 text-pink-400 px-3 py-1 rounded-lg">
+                        <img src="/src/img/Download.png" alt="" />
+                        <p>Import</p>
+                    </button>
+
+                    <button className="flex mr-6 border border-pink-400  hover:bg-pink-200 text-pink-400 px-3 py-1 rounded-lg">
+                        <img src="/src/img/Move up.png" alt="" />
+                        <p>Export</p>
+                    </button>
+
+                </div>
+            </div>
+
+            <div className="">
+                <div className="card">
+                    <DataTable
+                        value={customers}
+                        paginator rows={4}
+                        rowsPerPageOptions={[5, 10, 25, 50]}
+                        tableStyle={{ minWidth: '50rem' }}
+                        scrollable // Kích hoạt chế độ cuộn
+                        scrollHeight="400px" // Đặt chiều cao cố định cho bảng
+                        selection={selectedCustomers} // gắn state cho checkbox
+                        onSelectionChange={(e) => setSelectedCustomers(e.value)} // cập nhật state khi chọn
+                    >
+                        <Column selectionMode="multiple" headerStyle={{ width: '3em' }}></Column>
+                        <Column field="avatar" header="Avatar" body={avatarBodyTemplate} style={{ width: '10%' }}></Column>
+                        <Column field="name" header="Name" style={{ width: '20%' }}></Column>
+                        <Column field="company" header="Company" style={{ width: '20%' }}></Column>
+                        <Column field="order_value" header="Order value" style={{ width: '15%' }}></Column>
+                        <Column field="order_date" header="Order Date" style={{ width: '15%' }}></Column>
+                        <Column field="status" header="Status" style={{ width: '10%' }}></Column>
+                        <Column body={editBodyTemplate} headerStyle={{ width: '5%' }}></Column>
+
+                    </DataTable>
+                </div>
+
+            </div>
+        </>
+
+
+    )
 }
 
 export default Detailed;
