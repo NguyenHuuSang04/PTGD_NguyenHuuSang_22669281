58fc9620c4c6757a98c738df73ad57cf23757ad6
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-04-11 10:26:32 +0700

    6.isActive (NavLink React Router)

--- Tuan_07/src/App.jsx
+++ Tuan_07/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Projects from "./components/Projects/Projects";
+import Teams from "./components/Teams/Teams";
+import Analytics from "./components/Analytics/Analytics";
+import Messages from "./components/Messages/Messages";
+import Integrations from "./components/Integrations/Integrations";
 import 'primereact/resources/themes/lara-light-blue/theme.css';  // hoặc theme khác
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -12,6 +17,12 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} /> {/* Single Page Application */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/teams" element={<Teams />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/integrations" element={<Integrations />} />
     </Routes>
   );
 }
--- Tuan_07_2/src/App.jsx
+++ Tuan_07_2/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import Projects from "./Components/Projects/Projects";
+import Teams from "./Components/Teams/Teams";
+import Analytics from "./Components/Analytics/Analytics";
+import Messages from "./Components/Messages/Messages";
+import Integrations from "./Components/Integrations/Integrations";
 import 'primereact/resources/themes/lara-light-blue/theme.css';  // hoặc theme khác
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -10,6 +15,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} /> {/* Single Page Application */}
+      <Route path="/" element={<Dashboard />} /> {/* Single Page Application */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/teams" element={<Teams />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/integrations" element={<Integrations />} />
     </Routes>
   );
 }
